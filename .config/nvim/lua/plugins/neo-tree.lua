11bbfcf5ffc0360c270ddb848630af13bd9bdfe7
Laurent Chau <mplaurent.chau@gmail.com>
2024-10-08 22:16:10 +0200

    fix: disable hidden file

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -2,6 +2,13 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignore = false,
+          visible = true,
+        },
+      },
       window = {
         position = "right",
       },
