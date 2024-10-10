520f4be4e6b482af8cd39037b38c09e3412db9e0
Laurent Chau <mplaurent.chau@gmail.com>
2024-10-10 10:16:53 +0200

    fix: setup biome lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -7,6 +7,8 @@ return {
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
+      lspconfig.biome.setup({})
+
       lspconfig.lua_ls.setup({
         settings = {
           Lua = {
