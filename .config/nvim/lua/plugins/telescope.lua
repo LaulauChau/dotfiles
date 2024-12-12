d8013131dfd3282c79d93b833df9276ef3ed7346
Laurent Chau <mplaurent.chau@gmail.com>
2024-12-12 11:51:37 +0100

    feat(nvim): add multigrep

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,29 +2,34 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     branch = "0.1.x",
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+          },
+        },
+        extensions = {
+          fzf = {},
+        },
+      })
+
+      require("telescope").load_extension("fzf")
+
+      vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files)
+      vim.keymap.set("n", "<leader>fg", require("config.telescope.multigrep"))
+      vim.keymap.set("n", "<leader>fb", require("telescope.builtin").buffers)
+      vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags)
+    end,
     dependencies = {
       "nvim-lua/plenary.nvim",
       { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    },
-    keys = {
-      { "<leader>ff", "<CMD>Telescope find_files<CR>", desc = "Telescope find files" },
-      { "<leader>fg", "<CMD>Telescope live_grep<CR>", desc = "Telescope live grep" },
-      { "<leader>fb", "<CMD>Telescope buffers<CR>", desc = "Telescope buffers" },
-      { "<leader>fh", "<CMD>Telescope help-tags<CR>", desc = "Telescope help tags" },
-    },
-    opts = {
-      defaults = {
-        vimgrep_arguments = {
-				  "rg",
-					"-L",
-					"--color=never",
-					"--no-heading",
-					"--with-filename",
-					"--line-number",
-					"--column",
-					"--smart-case",
-				},
-      },
     },
   },
 }
