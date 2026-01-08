7a8fb095f45f4a1a4b74b2ad17c78d6299a2ba83
Laurent Chau <laurent.chau@proton.me>
2026-01-08 15:40:05 +0100

    don't need the custom implementation, can run rg flag directly in picker

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -43,23 +43,9 @@ return {
 			{
 				"<leader>fg",
 				function()
-					Snacks.picker.grep({
-						live = true,
-						title = "Multi Grep",
-						on_change = function(picker)
-							local input = picker.input.text
-							-- Split by double space to handle pattern + glob
-							local pieces = vim.split(input, "  ", { plain = true })
-
-							-- Dynamically update the ripgrep arguments
-							picker:set_filter({
-								search = pieces[1] or "",
-								glob = pieces[2] or "",
-							})
-						end,
-					})
+					Snacks.picker.grep({})
 				end,
-				desc = "Multi Grep (pattern  glob)",
+				desc = "Multi Grep",
 			},
 		},
 	},
